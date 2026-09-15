ca7a23a176782750cba4502cefe4384afcb2470c
Ivan Binev <ibinev@consultcommerce.com>
2026-09-15 10:28:50 +0300

    small fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v8';
+const VERSION = 'v9';
 const CACHE = 'botc-' + VERSION;
 // Derive base path so this works at root (localhost) or a subpath (GitHub Pages)
 const BASE = self.location.pathname.replace(/\/sw\.js$/, '') || '';
